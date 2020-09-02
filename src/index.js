c7ba6ef5d952693608d1f8451b35ff1722ea5434
dhillon64 <67638060+dhillon64@users.noreply.github.com>
2020-09-02 17:44:52 +0100

    added redux to project to handle state of sign in, finished google oauth sign in implementation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './Components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./Components/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import reducers from "./reducers";
+
+const store = createStore(reducers);
 
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
 );
