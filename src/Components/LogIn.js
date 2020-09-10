161f27cd42e83655a0d4c153c49df526559e09e4
dhillon64 <67638060+dhillon64@users.noreply.github.com>
2020-09-10 17:58:41 +0100

    added some styling to login page

--- src/Components/LogIn.js
+++ src/Components/LogIn.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 const LogIn = () => {
-  return <div>Please Sign In</div>;
+  return (
+    <div
+      class="ui centered raised card red"
+      style={{
+        marginTop: "50px",
+        textAlign: "center",
+      }}
+    >
+      <div class="content">
+        <div class="header">
+          Please Log In with A Google Account
+          <br />
+          <i className="google icon" />
+        </div>
+        <div class="description">
+          <p></p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LogIn;
